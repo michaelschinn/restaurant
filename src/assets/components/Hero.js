54520e3b096c42d4285ac8b27c80a24a3c7a8f38
michaelschinn <michael.stephen.chinn@gmail.com>
2021-10-14 13:11:49 -0400

    add bootstrap js, updated navigation and hero components

--- src/assets/components/Hero.js
+++ src/assets/components/Hero.js
@@ -6,22 +6,30 @@ export default function Hero()
         <>
             <section>
                 <article>
-                    <div id="hero" class="carousel slide" data-ride="carousel">
-                      <ol class="carousel-indicators">
-                        <li data-target="#hero" data-slide-to="0" class="active"></li>
-                        <li data-target="#hero" data-slide-to="1"></li>
-                        <li data-target="#hero" data-slide-to="2"></li>
+                    <div id="hero" className="carousel slide" data-bs-ride="carousel">
+                      <ol className="carousel-indicators">
+                        <li data-bs-target="#hero" data-bs-slide-to="0" className="active"></li>
+                        <li data-bs-target="#hero" data-bs-slide-to="1"></li>
+                        <li data-bs-target="#hero" data-bs-slide-to="2"></li>
                       </ol>
-                      <div class="carousel-inner">
-                        b5-carousel-item
+                      <div className="carousel-inner">
+                            <div className="carousel-item active">
+                              <div className="bg-dark text-white text-center display-1 py-5">Slide 1 goes here.</div>
+                            </div>
+                            <div className="carousel-item">
+                              <div className="bg-dark text-white text-center display-1 py-5">Slide 2 goes here.</div>
+                            </div>
+                            <div className="carousel-item">
+                              <div className="bg-dark text-white text-center display-1 py-5">Slide 3 goes here.</div>
+                            </div>
                       </div>
-                      <a class="carousel-control-prev" href="#hero" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
+                      <a className="carousel-control-prev" href="#hero" role="button" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
                       </a>
-                      <a class="carousel-control-next" href="#hero" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
+                      <a className="carousel-control-next" href="#hero" role="button" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
                       </a>
                     </div>
                 </article>
--- src/assets/components/Navigation.js
+++ src/assets/components/Navigation.js
@@ -4,11 +4,11 @@ export default function Navigation()
 {
     return( 
         <>
-            <nav>
-                <ul>
-                    <li>Menu</li>
-                    <li>About</li>
-                    <li>Location</li>
+            <nav className="">
+                <ul className="nav">
+                    <li className="nav-item">Hero</li>
+                    <li className="nav-item">About</li>
+                    <li className="nav-item">Location</li>
                 </ul>
             </nav>
         </>
